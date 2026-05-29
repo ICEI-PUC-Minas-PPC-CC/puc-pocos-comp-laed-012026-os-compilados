4fa94b9eb931086696477d873c6ab83cdf245954
Pablo Oliveira <pablo.oliveira.1638169@sga.pucminas.br>
2026-05-29 12:01:11 -0300

    GERENCIADOR DE TAREFAS.c

--- codigos/base.c
+++ codigos/base.c
@@ -1,23 +1,42 @@
+* PROJETO DE AED I - ETAPA 1 (Entrega: 02/06)
+ * TÍTULO: Gerenciador de Tarefas
+ * GRUPO: Pablo, Daniel, Ronaldo
+
+CÓDIGO EM C:
+
 #include <stdio.h>
-//GERENCIADOR DE TAREFAS
-int main(){
-    int op;
+#include <stdlib.h>
 
+// 1. ESTRUTURA DO CÓDIGO DE ACORDO COM REQUISITOS DO DIEGO
 
+struct Tarefa {
+    int codigo;
+    char nome[50];
+    int ativa; // 1 para ativa, 0 para excluida
+};
 
+int main() {
+    // 2. requisito vetor com capacidade para 10 tarefas
+    struct Tarefa lista[10];
+    int opcao;
+    int i;
+
+    //precisamos definir o for aqui, começar ele zerado vazio <--
+    
+    // 3. laço principal do menu
     do{
-        printf("========BEM VINDO AO GERENCIADOR DE TAREFAS SELECIONE UMA OPCAO ABAIXO========\n");
-        printf("1 - Incluir tarefa: \n");
-        printf("2 - Agendar tarefa: \n");
-        printf("3 - Editar tarefa: \n");
-        printf("4 - Concluir tarefa: \n");
-        printf("5 - Excluir tarefa: \n");
-        printf("0 - Fechar o sistema: \n");
+        printf("\n--- GERENCIADOR DE TAREFAS ---\n");
+        printf("1. - INSERIR TAREFA \n");
+        printf("2. - LISTAR TAREFA \n");
+        printf("3. - EDITAR TAREFA \n");
+        printf("4. - EXLUIR TAREFA \n");
+        printf("5. - PESQUISAR TAREFA \n");
+        printf("0. - LIMPAR SISTEMA/TAREFAS \n");
 
         printf("Digite uma opcao: ");
-        scanf(" %d", &op);
+        scanf(" %d", &opção);
 
-        switch (op){
+        switch (opção){
             case 1:
                 printf("Incluindo...\n\n");
                 break;
@@ -41,15 +60,7 @@ int main(){
                 break;
 
         }
-    }while(op != 0);
+    }while(opção != 0);
 
     return 0;
 }
-
-
-//agendar tarefa
-//concluir
-//incluir tar
-//excluir tarefa
-//editar tarefa
-//FECHAR O BAGUI
